82a904260ed81a2e817f523dc945069ea12957af
Álvaro Fernández Amaro <alvaroofernaandez@gmail.com>
2024-10-10 13:57:17 +0200

    logica principal del juego creada

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,137 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const TURNOS = {
+  X: '❌',
+  O: '⭕'
+}
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+  
+  const handleClick = () => {
+    updateBoard(index)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
+function App() {
+  const [board, setBoard] = useState(
+    Array(9).fill(null)
+  )
+
+  const[turno, setTurno] = useState(TURNOS.X)
+  const[winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    return null
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurno(TURNOS.X)
+    setWinner(null)
+  }
+
+  const updateBoard = (index) => {
+    if (board[index] || winner) return
+
+    const newBoard = [...board]
+    newBoard[index] = turno
+    setBoard(newBoard)
+
+    const newTurno = turno === TURNOS.X ? TURNOS.O : TURNOS.X
+    setTurno(newTurno)
+
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+
+    }
+  }
+
+  return (
+    <main className='board'>
+      <h1>3 IN ROW</h1>
+      <section className='game'>
+      {
+        board.map((_, index) => {
+          return (
+            <Square
+              key={index}
+              index={index}
+              updateBoard={updateBoard}
+            >
+              {board[index]}
+            </Square>
+          )
+        })
+      }
+      </section>
+
+      <section className='turn'>
+        <Square isSelected={turno === TURNOS.X}>
+          {TURNOS.X}
+        </Square>
+        <Square isSelected={turno === TURNOS.O}>
+          {TURNOS.O}
+        </Square>
+      </section>
+
+      
+        {
+          winner !== null && (
+            <section className='winner'>
+              <div className='text'>
+                <h2>
+                {
+                  winner === false
+                    ? 'Empate'
+                    : 'Ganó: '
+                }
+                </h2>
+
+                <header className="win">
+                  {winner && <Square>{winner}</Square>}
+                </header>
+
+                <footer>
+                  <button onClick={resetGame}>Resetear</button>
+                </footer>
+
+              </div>
+            </section>
+          )
+        }
+
+    </main> 
   )
 }
 
